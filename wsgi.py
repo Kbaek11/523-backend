fbfee4daf7595fec7c0dde4c80b9cfe7dfab0f07
Linh Ly <lly23@live.unc.edu>
2018-03-26 20:53:40 -0400

    update

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:522314@localhost/postgres'
+db = SQLAlchemy(application)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 @application.route('/')
 def main():
