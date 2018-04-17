8aaa166655af80b9568666b2f2d8d3d94cdcc6c8
Kyung Baek <kbaek11@live.unc.edu>
2018-04-17 13:28:25 -0400

    Disabled pylint errors

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort  #pylint: disable=E0401
 from models import db, Users, UserAnswers
-from flask_migrate import Migrate
-from dotenv import load_dotenv
+from flask_migrate import Migrate  #pylint: disable=E0401
+from dotenv import load_dotenv  #pylint: disable=E0401
 from os.path import join, dirname
 import os
-import psycopg2
+import psycopg2  #pylint: disable=E0401
 
 #Initialize Flask, SQLAlchemy, Migrations
 application = Flask(__name__)
@@ -31,7 +31,7 @@ def addUser():
         if not request.json:
             abort(400)
         payload = request.json
-        if not 'userId' in payload or not 'team' in payload:
+        if 'userId' not in payload or 'team' not in payload:
             raise Exception('Missing "userId" or "team" in JSON')
         newUser = Users(payload['userId'], payload['team'])
         try:
@@ -54,20 +54,20 @@ def addUserAnswers():
     if request.method == 'POST':
         if not request.json:
             abort(400)
-        j = request.json
-        if not 'userId' in j:
+        payload = request.json
+        if 'userId' not in payload:
             raise Exception('Missing "userId" in JSON')
         userAnswers = UserAnswers(
-            j['userId'], j['day1a'], j['day1b'], j['day1c'], j['day2a'],
-            j['day2b'], j['day2c'], j['day3a'], j['day3b'], j['day3c'],
-            j['day4a'], j['day4b'], j['day4c'], j['day5a'], j['day5b'],
-            j['day5c'], j['day6a'], j['day6b'], j['day6c'], j['day7a'],
-            j['day7b'], j['day7c'], j['day8a'], j['day8b'], j['day8c'],
-            j['day9a'], j['day9b'], j['day9c'], j['day10a'], j['day10b'],
-            j['day10c'], j['day11a'], j['day11b'], j['day11c'], j['day12a'],
-            j['day12b'], j['day12c'], j['day13a'], j['day13b'], j['day13c'],
-            j['day14a'], j['day14b'], j['day14c'], j['q1'], j['q2'], j['q3'],
-            j['q4'], j['q5'], j['q6'], j['q7'], j['q8'], j['q9'], j['q10'])
+            payload['userId'], payload['day1a'], payload['day1b'], payload['day1c'], payload['day2a'],
+            payload['day2b'], payload['day2c'], payload['day3a'], payload['day3b'], payload['day3c'],
+            payload['day4a'], payload['day4b'], payload['day4c'], payload['day5a'], payload['day5b'],
+            payload['day5c'], payload['day6a'], payload['day6b'], payload['day6c'], payload['day7a'],
+            payload['day7b'], payload['day7c'], payload['day8a'], payload['day8b'], payload['day8c'],
+            payload['day9a'], payload['day9b'], payload['day9c'], payload['day10a'], payload['day10b'],
+            payload['day10c'], payload['day11a'], payload['day11b'], payload['day11c'], payload['day12a'],
+            payload['day12b'], payload['day12c'], payload['day13a'], payload['day13b'], payload['day13c'],
+            payload['day14a'], payload['day14b'], payload['day14c'], payload['q1'], payload['q2'], payload['q3'],
+            payload['q4'], payload['q5'], payload['q6'], payload['q7'], payload['q8'], payload['q9'], payload['q10'])
 
         try:
             db.session.add(userAnswers)
