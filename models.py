08beef61367f515f666681add293d7651de4b8c6
Kyung Baek <kbaek11@live.unc.edu>
2018-04-17 20:33:51 -0400

    Updated models serialize

--- models.py
+++ models.py
@@ -10,15 +10,17 @@ class Users(db.Model):
     userId = db.Column(db.Integer, primary_key=True)
     team = db.Column(db.String(80))
 
-    def __init__(self, userId, team):
-        self.userId = userId
-        self.team = team
-
     def serialize(self):
         return {
             'userId': self.userId,
             'team': self.team,
         }
+
+    def __init__(self, userId, team):
+        self.userId = userId
+        self.team = team
+
+
 
 
 class UserAnswers(db.Model):
@@ -81,6 +83,64 @@ class UserAnswers(db.Model):
     q8 = db.Column(db.Boolean)
     q9 = db.Column(db.Boolean)
     q10 = db.Column(db.Boolean)
+
+    def serialize(self):
+        return {
+            'userId': self.userId,
+            'day1a': self.day1a,
+            'day1b': self.day1b,
+            'day1c': self.day1c,
+            'day2a': self.day2a,
+            'day2b': self.day2b,
+            'day2c': self.day2c,
+            'day3a': self.day3a,
+            'day3b': self.day3b,
+            'day3c': self.day3c,
+            'day4a': self.day4a,
+            'day4b': self.day4b,
+            'day4c': self.day4c,
+            'day5a': self.day5a,
+            'day5b': self.day5b,
+            'day5c': self.day5c,
+            'day6a': self.day6a,
+            'day6b': self.day6b,
+            'day6c': self.day6c,
+            'day7a': self.day7a,
+            'day7b': self.day7b,
+            'day7c': self.day7c,
+            'day8a': self.day8a,
+            'day8b': self.day8b,
+            'day8c': self.day8c,
+            'day9a': self.day9a,
+            'day9b': self.day9b,
+            'day9c': self.day9c,
+            'day10a': self.day10a,
+            'day10b': self.day10b,
+            'day10c': self.day10c,
+            'day11a': self.day11a,
+            'day11b': self.day11b,
+            'day11c': self.day11c,
+            'day12a': self.day12a,
+            'day12b': self.day12b,
+            'day12c': self.day12c,
+            'day13a': self.day13a,
+            'day13b': self.day13b,
+            'day13c': self.day13c,
+            'day14a': self.day14a,
+            'day14b': self.day14b,
+            'day14c': self.day14c,
+            'q1': self.q1,
+            'q2': self.q2,
+            'q3': self.q3,
+            'q4': self.q4,
+            'q5': self.q5,
+            'q6': self.q6,
+            'q7': self.q7,
+            'q8': self.q8,
+            'q9': self.q9,
+            'q10': self.q10
+        }
+    
 
     def __init__(self, userId, day1a, day1b, day1c, day2a, day2b, day2c, day3a,
                  day3b, day3c, day4a, day4b, day4c, day5a, day5b, day5c, day6a,
